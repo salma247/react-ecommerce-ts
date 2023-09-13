711865d756136519775d2af5963f69f12eeff504
salma247 <salmaelhamshary1@gmail.com>
2023-09-13 19:20:26 +0300

    fix: fix scroll behaviour

--- src/components/product/ProductPagination.tsx
+++ src/components/product/ProductPagination.tsx
@@ -11,28 +11,30 @@ type Props = {
 const ProductPagination = ({ page, pages, handlePageChange, data }: Props) => {
   const containerRef = useRef<HTMLDivElement | null>(null);
 
-  const handleScrollToPage = (page: number) => {
-    const container = containerRef.current;
-    if (container) {
-      const children = container.children;
-      const target = children[page - 1] as HTMLButtonElement;
-      target.scrollIntoView({ behavior: 'smooth' });
+  const scrollToPage = () => {
+    if (containerRef.current) {
+      //scroll right to the active page
+      containerRef.current.scrollLeft =
+        (page - 1) * containerRef.current.children[0].clientWidth;
+
+      //scroll to the start if the active page is the first page
+      if (page === 1) {
+        containerRef.current.scrollLeft = 0;
+      }
     }
-  }
+  };
 
   const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   useEffect(() => {
-    handleScrollToPage(page);
+    scrollToPage();
     scrollToTop();
-  }
-  , [page]);
+  }, [page]);
 
   return (
-    <div className="flex justify-center items-center space-x-4 mt-8">
-      
+    <div className="flex justify-center items-center space-x-4 mt-8 w-1/2 mx-auto"> 
         <button
           className="px-2 py-1 bg-gray-100 rounded-md disabled:opacity-50"
           disabled={page === 1}
@@ -40,7 +42,7 @@ const ProductPagination = ({ page, pages, handlePageChange, data }: Props) => {
         >
           <FaChevronLeft />
         </button>
-        <div className="flex space-x-2 w-1/2 overflow-hidden" ref={containerRef}>
+        <div className="flex space-x-2 overflow-hidden" ref={containerRef}>
           {Array.from({ length: pages }).map((_, i) => (
             <button
               key={i}
