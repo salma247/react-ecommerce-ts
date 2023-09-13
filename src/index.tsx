c712de1e03cc08f700e5f6dc43e301872b38f2c2
salma247 <salmaelhamshary1@gmail.com>
2023-09-13 15:30:52 +0300

    fixup! chore: add gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-ecommerce-ts">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
